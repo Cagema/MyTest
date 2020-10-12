e8d5f38e2fee64a6450b54d6c4a827a52399e479
Максим Шалашников <Schalaschnikov@yandex.ru>
2020-10-12 12:23:41 +0300

    Add networkService

---
+++ test/NetworkService.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  test
+//
+//  Created by Максим Шалашников on 12.10.2020.
+//  Copyright © 2020 Максим Шалашников. All rights reserved.
+//
+
+import Foundation
+
+class NetworkService {
+    
+}
